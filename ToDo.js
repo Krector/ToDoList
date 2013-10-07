6122303495c6f5c3459fb73b020787eb8bb4c45f
Krector <k.rector@w3insites.com>
2013-10-07 14:21:57 -0600

    Added js skeleton, changed html and css to support un-ordered lists

---
+++ ToDo.js
@@ -0,0 +1,59 @@
+
+/*          ToDo.js
+ * Contains the functions required to implement a to do list that performs the following:
+ * Allows the customer to:
+ *  Add a task by typing it into the input line
+ *  Complete a task by clicking the check mark to the left of a task
+ *  Reactivate a task by clicking on the check mark to the left of a completed task
+ *  Edit a task by double clicking it
+ *  Delete a task by clicking on the "X" at the end of the task
+ *  View tasks by category by selecting All, Active, or Completed from the status line 
+
+ */
+"use strict";
+
+/* 
+ * Add a task to the To Do list
+ * 
+ */
+var addTask = function (text) {
+    task = {};
+    return task;
+}
+
+/* 
+ * Set the status of a task
+ * 
+ */
+var completeTask = function (task) {
+
+    return task;
+}
+
+/* 
+ * Edit a task
+ * 
+ */
+var editTask = function (task) {
+
+    return task;
+}
+
+/* 
+ * Delete the task
+ * 
+ */
+var deleteTask = function (task) {
+
+}
+
+/* 
+ * Display selected tasks
+ * 
+ */
+var displayTasks = function (status) {
+    var taskIndexList [];
+
+   return taskIndexList
+}
+
