d9b4d84d7682b6fc17caf24ee53fa09e4a6e4c39
Krector <k.rector@w3insites.com>
2013-10-14 14:46:50 -0600

    Task line and checkmark float correctly. Add new task works

--- ToDo.js
+++ ToDo.js
@@ -1,59 +1,74 @@
 
 /*          ToDo.js
- * Contains the functions required to implement a to do list that performs the following:
+ * Contains the code required to implement a to do list that
+ *   performs the following functions:
  * Allows the customer to:
  *  Add a task by typing it into the input line
- *  Complete a task by clicking the check mark to the left of a task
- *  Reactivate a task by clicking on the check mark to the left of a completed task
+ *  Toggle the task status (complete / active) clicking the check mark
+ *      to the left of the task
  *  Edit a task by double clicking it
  *  Delete a task by clicking on the "X" at the end of the task
- *  View tasks by category by selecting All, Active, or Completed from the status line 
-
+ *  Toggle the status of all tasks by clicking the down pointing
+ *      chevron to the left of the task input line
+ *  View tasks by category by selecting All, Active, or Completed from
+ *      the task status line
  */
 "use strict";
 
-/* 
- * Add a task to the To Do list
- * 
- */
-var addTask = function (text) {
-    task = {};
-    return task;
+var app = {
+    
+    /* 
+    * Register the required JQuery event callbacks
+    */
+    
+    init: function (){
+        $(".InputTask").keypress(app.checkKeypress);
+        $(".TaskList").on("click", ".toggle", app.toggleStatus);
+        
+    },
+
+    checkKeypress : function(ev) {
+       if (ev.which === 13){
+           app.addTask();
+       }
+     },
+    
+    addTask : function () {
+
+        // Clone a new task from the template
+        var newTask = $(".templates li").clone();
+        
+        // Update the new task text with the input text
+//        var inputText= $(".InputTask").val();
+        newTask.find("span.TaskText").text( $(".InputTask").val());
+      // complete new item if needed
+/*
+        if(status === 'complete') {
+            newItem.addClass('complete');
+        }
+*/
+// append the new task to the list
+        newTask.appendTo(".TaskList");
+        // Blank out the task input
+        $(".InputTask").val('');
+
+
+    },
+    
+    toggleStatus : function (ev) {
+        
+    },
+    
+    editTask : function () {
+    },
+    
+    deleteTask : function () {
+    },
+    
+    toggleAllStatus : function () {
+    },
+    
+    viewSelectedTasks : function (filterTask) {
+    }
+
 }
-
-/* 
- * Set the status of a task
- * 
- */
-var completeTask = function (task) {
-
-    return task;
-}
-
-/* 
- * Edit a task
- * 
- */
-var editTask = function (task) {
-
-    return task;
-}
-
-/* 
- * Delete the task
- * 
- */
-var deleteTask = function (task) {
-
-}
-
-/* 
- * Display selected tasks
- * 
- */
-var displayTasks = function (status) {
-    var taskIndexList [];
-
-   return taskIndexList
-}
-
